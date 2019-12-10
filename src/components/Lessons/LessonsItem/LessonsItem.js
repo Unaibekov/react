98fc7cdf82e6fa3c4b7300f46fcc713fb7989369
Ildar Unaybekov <i.unaybekov@zenit.ru>
2019-12-10 16:58:13 +0300

    props

---
+++ src/components/Lessons/LessonsItem/LessonsItem.js
@@ -0,0 +1,20 @@
+import React from 'react'; 
+import htmlBg from '../../../assets/images/htmlBg.jpg'
+
+import styles from '../LessonsItem/LessonItems.module.css'; 
+
+const LessonsItem=(props) => {
+    return (
+        <div className={styles.item}>
+            <i className={styles.line}></i>
+            <img src={htmlBg} alt='htmlBg' />
+            <div className={styles.overlay}></div>
+            <div className={styles.title}>{props.title}</div>
+            <svg className={styles.icon} viewbox="0 0 28 25">
+                <path fill="#fff" d="M13.145 2.13l1.94-1.867 12.178 12-12.178 12-1.94-1.867 8.931-8.8H.737V10.93h21.339z" />
+            </svg>
+        </div>
+    )
+}
+
+export default LessonsItem;
