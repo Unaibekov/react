cdf158ba243d2de0e31fe79fdfd3bb319366d860
Unaibekov <uin-a@mail.ru>
2019-12-19 21:57:55 +0400

    q

---
+++ build/precache-manifest.f50db46611aa9eaecba39ad9f1129604.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e1c22f723accf6284e479fa009a5960",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "6b666f15db55c912e480",
+    "url": "/react/static/css/2.44ea01ec.chunk.css"
+  },
+  {
+    "revision": "7223635f66e2710fc41f",
+    "url": "/react/static/css/main.d843a840.chunk.css"
+  },
+  {
+    "revision": "6b666f15db55c912e480",
+    "url": "/react/static/js/2.9c8ecd16.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react/static/js/2.9c8ecd16.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7223635f66e2710fc41f",
+    "url": "/react/static/js/main.30b633b4.chunk.js"
+  },
+  {
+    "revision": "5167de68795de59d241c",
+    "url": "/react/static/js/runtime-main.cbcb79aa.js"
+  },
+  {
+    "revision": "2c03cc1889d364f3771163f5e0ceb3e9",
+    "url": "/react/static/media/cssBg.2c03cc18.jpg"
+  },
+  {
+    "revision": "35965b29daf65cdd8026e1979cfeea76",
+    "url": "/react/static/media/htmlBg.35965b29.jpg"
+  },
+  {
+    "revision": "b7608a658af2ba86ed2c400447234ce3",
+    "url": "/react/static/media/javaBg.b7608a65.png"
+  },
+  {
+    "revision": "ed69dc44c31fcf19d5101730f657c01d",
+    "url": "/react/static/media/leninBg.ed69dc44.jpg"
+  },
+  {
+    "revision": "91f551a1ed6bf78427aa2d5ef0420925",
+    "url": "/react/static/media/menu.91f551a1.svg"
+  },
+  {
+    "revision": "518d4ccc23ac64c461a5c7b4048c0a93",
+    "url": "/react/static/media/reactBg.518d4ccc.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6ab93e35ca3feb234512168d93497fb6.js"
+  "/react/precache-manifest.f50db46611aa9eaecba39ad9f1129604.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
