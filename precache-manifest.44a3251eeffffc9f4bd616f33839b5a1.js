a22612bc9c4d165845e5129c4d33c2b4d78cfba1
Ildar Unaybekov <i.unaybekov@zenit.ru>
2020-02-10 10:32:19 +0300

    Updates

---
+++ precache-manifest.44a3251eeffffc9f4bd616f33839b5a1.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5aed1bb6f03566ebdb3c370449fa2c",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "ca1edce29ab17d92c7e8",
+    "url": "/react/static/css/2.44ea01ec.chunk.css"
+  },
+  {
+    "revision": "d567fb828d58a407d4dc",
+    "url": "/react/static/css/main.0505da17.chunk.css"
+  },
+  {
+    "revision": "ca1edce29ab17d92c7e8",
+    "url": "/react/static/js/2.0f35748c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/react/static/js/2.0f35748c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d567fb828d58a407d4dc",
+    "url": "/react/static/js/main.0423885a.chunk.js"
+  },
+  {
+    "revision": "5167de68795de59d241c",
+    "url": "/react/static/js/runtime-main.cbcb79aa.js"
+  },
+  {
+    "revision": "2c03cc1889d364f3771163f5e0ceb3e9",
+    "url": "/react/static/media/cssBg.2c03cc18.jpg"
+  },
+  {
+    "revision": "d493bd5295c06cd8c3e98a8be9a93efc",
+    "url": "/react/static/media/html.d493bd52.png"
+  },
+  {
+    "revision": "35965b29daf65cdd8026e1979cfeea76",
+    "url": "/react/static/media/htmlBg.35965b29.jpg"
+  },
+  {
+    "revision": "b7608a658af2ba86ed2c400447234ce3",
+    "url": "/react/static/media/javaBg.b7608a65.png"
+  },
+  {
+    "revision": "03d471c6a43c17a3496228b5b43512e6",
+    "url": "/react/static/media/js.03d471c6.png"
+  },
+  {
+    "revision": "ed69dc44c31fcf19d5101730f657c01d",
+    "url": "/react/static/media/leninBg.ed69dc44.jpg"
+  },
+  {
+    "revision": "518d4ccc23ac64c461a5c7b4048c0a93",
+    "url": "/react/static/media/reactBg.518d4ccc.png"
+  },
+  {
+    "revision": "5b4efb1420f64da9a8fba88799a40a25",
+    "url": "/react/static/media/reactjs.5b4efb14.png"
+  },
+  {
+    "revision": "350f7e195aa51ddf90ca9676a2e47feb",
+    "url": "/react/static/media/redux.350f7e19.png"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/react/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/react/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/react/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/react/static/media/slick.f97e3bbf.svg"
+  }
+]);
