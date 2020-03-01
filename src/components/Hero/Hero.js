36e5855c6109b68c06da8514be6d6ef39818b6c2
Unaibekov <uin-a@mail.ru>
2020-03-01 18:30:41 +0400

    hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,27 +1,11 @@
 import React from 'react';
 import styles from '../Hero/Hero.module.css';
-import { useSelector } from 'react-redux';
 import logo from '../../assets/images/logo.svg'
 
 export const Hero = () => {
-  const svgs = useSelector(state => state.svgs)
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 500,
-      behavior: 'smooth'
-    });
-  }
-  
   return (
     <div className={styles.main}>
-        <svg
-          width="26" height="40" viewBox="0 0 26 40"
-          className={styles.down}
-          onClick={() => scrollToTop()}
-        >
-          <path d={svgs.scrollDown} />
-        </svg>
-      {/* <div className={styles.title}>Учиться,<br />учиться<br />и еще раз<br />УЧИТЬСЯ!!!</div> */}
+      <div className={styles.down}><span></span></div>
       <img src={logo} className={styles.logo} alt='logo' />
     </div>
   )
